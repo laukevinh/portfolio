9ddec79ef02534427bf3ae3fe6705fd8972b7056
klau <laudevelopment@gmail.com>
2021-10-18 23:21:05 -0500

    Add ProjectsContainer and ContactsContainer to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import './App.css';
+import ContactsContainer from './components/ContactsContainer';
 import HeadlineContainer from './components/HeadlineContainer';
+import ProjectsContainer from './components/ProjectsContainer';
 
 function App() {
   return (
     <Container fluid>
       <HeadlineContainer />
+      <ProjectsContainer />
+      <ContactsContainer />
     </Container>
   );
 }
