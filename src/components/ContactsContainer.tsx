a94a669d3d51abb6265e1212d8838ed3a65192d3
klau <laudevelopment@gmail.com>
2021-10-19 22:26:01 -0500

    Lift contacts data into separate file

--- src/components/ContactsContainer.tsx
+++ src/components/ContactsContainer.tsx
@@ -1,32 +1,28 @@
-import { Button, Container, Grid, Header, Image } from "semantic-ui-react";
+import { Button, Container, Header } from "semantic-ui-react";
+import contactsData from "../data/contacts.json";
 
 function ContactsContainer() {
+  const { email, github, linkedin } = contactsData;
+
   return (
-    <Container id='contacts'>
-      <Grid style={{ minHeight: 800 }}>
-        <Grid.Column width={12}>
-          <Header
-            as='h1'
-            style={{
-              fontSize: '4em',
-              marginTop: '3em',
-            }}
-          >
-            Contact me
-          </Header>
-          <Header
-            as='h2'
-            style={{ fontSize: '1.5em' }}
-          >
-            <a href='mailto: laukevinh@gmail.com'>laukevinh@gmail.com</a>
-          </Header>
-          <Button as='a' target='_blank' href='https://github.com/laukevinh'>Github</Button>
-          <Button as='a' target='_blank' href='https://www.linkedin.com/in/kalau/'>LinkedIn</Button>
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <Image>Placeholder</Image>
-        </Grid.Column>
-      </Grid>
+    <Container id='contacts' style={{ marginBottom: 300 }}>
+      <Header
+        as='h1'
+        style={{
+          fontSize: '4em',
+          marginTop: '3em',
+        }}
+      >
+        Contact me
+      </Header>
+      <Header
+        as='h2'
+        style={{ fontSize: '1.5em' }}
+      >
+        <a href={`'mailto: ${email}`}>{email}</a>
+      </Header>
+      <Button as='a' target='_blank' href={github}>Github</Button>
+      <Button as='a' target='_blank' href={linkedin}>LinkedIn</Button>
     </Container >
   );
 }
