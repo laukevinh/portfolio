3fb30ebbeefc64f3d6805c2c8e5a7c6a84c22caf
klau <laudevelopment@gmail.com>
2021-10-31 23:33:10 -0500

    Try to fix navigation bug by hardcoding basePath

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH + '/'
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/'
 }
 
 module.exports = nextConfig
